1857c534d65ddbc7824522eb3818a3e6007804d9
陶群 <793264946@qq.com>
2016-03-12 01:13:52 +0800

    完善菜单栏下拉效果
    完善菜单栏下拉效果

--- jscript/Jscript.js
+++ jscript/Jscript.js
@@ -1,8 +1,59 @@
-function show(){
-	var vv = document.getElementById("show");
-	vv.style.overflow = "visible"
-}
-function hidd(){
-	var vv = document.getElementById("show");
-	vv.style.overflow = "hidden"
+window.onload = function(){
+	var about = document.getElementById("hd_right");
+		about.onmouseover = function(){
+			var menu = document.getElementById("subnav");		
+			clearInterval(menu.time);
+			menu.time=setInterval(function(){
+			menu.style.height = menu.offsetHeight+15+"px";
+			if(menu.offsetHeight>=120){
+				clearInterval(menu.time);
+			}
+		},30)
+	}
+
+	var about = document.getElementById("hd_right");
+		about.onmouseout = function(){
+			var menu = document.getElementById("subnav");	
+			clearInterval(menu.time);
+			menu.time=setInterval(function(){
+			menu.style.height = menu.offsetHeight-15+"px";
+			if(menu.offsetHeight<=0){
+				clearInterval(menu.time);
+			}
+		},30)
+	}
+
+	var hd_menu = document.getElementsByTagName("li");
+	for(var i=0;i<hd_menu.length;i++){
+		hd_menu[i].onmouseover = function(){
+			var osb = this.getElementsByTagName("ul")[0];
+			if(osb){
+				var This = osb;
+				clearInterval(This.time);
+				This.time = setInterval(function(){
+					This.style.height = This.offsetHeight+10+"px";
+					if(This.offsetHeight>=200){
+						clearInterval(This.time);
+					}
+				},30)
+			}
+		}
+	}
+
+	var hd_menu = document.getElementsByTagName("li");
+	for(var i=0;i<hd_menu.length;i++){
+		hd_menu[i].onmouseout = function(){
+			var osb = this.getElementsByTagName("ul")[0];
+			if(osb){
+				var This = osb;
+				clearInterval(This.time);
+				This.time = setInterval(function(){
+					This.style.height = This.offsetHeight-10+"px";
+					if(This.offsetHeight<=0){
+						clearInterval(This.time);
+					}
+				},30)
+			}
+		}
+	}
 }
