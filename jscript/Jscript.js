62bbf070069fb6090b939cb917459b7785b09bb5
taoqun <793264946@qq.com>
2016-03-11 13:23:09 +0800

    增加鼠标指针显示，hover时隐藏

---
+++ jscript/Jscript.js
@@ -0,0 +1,8 @@
+function show(){
+	var vv = document.getElementById("show");
+	vv.style.overflow = "visible"
+}
+function hidd(){
+	var vv = document.getElementById("show");
+	vv.style.overflow = "hidden"
+}
