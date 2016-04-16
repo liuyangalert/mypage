148311952c601b320be8f1f68d91202694cccded
陶群 <793264946@qq.com>
2016-04-16 23:44:23 +0800

    增加 点击删除，判断输入的是否为数字

--- baidu_ife/task-18/script.js
+++ baidu_ife/task-18/script.js
@@ -1,16 +1,6 @@
-function addEvent(ele,event,handler){
-	if (ele.addEvenListener) {
-		ele.addEvenListener(event,handler,false);
-	}
-	else if (ele.attachEvent) {
-		ele.attachEvent("on"+event,handler);
-	}
-	else{
-		ele["on"+event] = handler;
-	}
-	}
-
 var num = 0;
+var re = /^[0-9]+.?[0-9]*$/;
+
 function getnum(){
 	num = document.getElementById("num").value;
 	}
@@ -21,24 +11,46 @@ function insert(){
 		getnum();
 		var x = content.getElementsByTagName("div")[0];
 		var n = document.createElement("div");
-		n.innerHTML = num;
-		content.insertBefore(n,x);
+		if (re.test(num)) {
+	 		n.innerHTML = num;
+			content.insertBefore(n,x);
+			del();
+		}else{
+			alert("请输入数字！");
+		}
 	}
 	insert[2].onclick = function(){
 		getnum();
 		var n = document.createElement("div");
-		n.innerHTML = num;
-		content.appendChild(n)
+		if (re.test(num)) {
+	 		n.innerHTML = num;
+			content.appendChild(n);
+			del();
+		}else{
+			alert("请输入数字！");
+		}
 	}
 	insert[3].onclick = function(){
 		var x = content.childNodes;
 		content.removeChild(x[0]);
+		del();
 	}
 	insert[4].onclick = function(){
 		var x = content.childNodes;
 		content.removeChild(x[x.length-1]);
+		del();
 	}
-}
+	function del(){
+	 	var dd = content.getElementsByTagName("div");
+	 	for(var i=0;i<dd.length;i++){
+	 		dd[i].onclick = function(){
+	 			var x = content.childNodes;
+				content.removeChild(x[i-1]);
+				del();
+	 		}
+	 	}
+	 }
+	}
 window.onload = function(){
 	insert();
 }
