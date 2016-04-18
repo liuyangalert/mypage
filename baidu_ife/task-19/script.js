005c10483430efe0636b73955683b7b8eaab1f1b
陶群 <793264946@qq.com>
2016-04-18 22:42:30 +0800

    第19题，还差一个可视化冒泡排序

--- baidu_ife/task-19/script.js
+++ baidu_ife/task-19/script.js
@@ -1,53 +1,77 @@
-var num = 0;
-var re = /^[0-9]+.?[0-9]*$/;
 
-function getnum(){
-	num = document.getElementById("num").value;
-	}
 function insert(){
 	var content = document.getElementById("content");
 	var insert = document.getElementsByTagName("input");
+	
+	var num = 0;
+	var re = /^[0-9]+.?[0-9]*$/;
+
+	function getnum(){
+		num = document.getElementById("num").value;
+		if (re.test(num)) {
+			if(num>100){
+				alert("请输入小于100的数字");
+				location.reload(true);//刷新此页面
+			}else if(num<10){
+				alert("请输入大于10的数字");
+				location.reload(true);//刷新此页面
+			}else{
+				return num;
+			}
+		}else {
+			alert("请输入数字");
+			location.reload(true);//刷新此页面
+		}
+	}
 	insert[1].onclick = function(){
 		getnum();
 		var n = document.createElement("div");
-		if (re.test(num)) {
-	 		n.innerHTML = num;
-			content.insertBefore(n,content.firstChild);
-			del();
-		}else{
-			alert("请输入数字！");
-		}
+		n.innerHTML = num;
+		n.style.height = num+"px";
+		content.insertBefore(n,content.firstChild);
+		pan();
+		del();
 	}
 	insert[2].onclick = function(){
 		getnum();
 		var n = document.createElement("div");
-		if (re.test(num)) {
-	 		n.innerHTML = num;
-			content.appendChild(n);
-			del();
-		}else{
-			alert("请输入数字！");
-		}
+ 		n.innerHTML = num;
+ 		n.style.height=num+"px";
+		content.appendChild(n);
+		pan();
+		del();
 	}
 	insert[3].onclick = function(){
-		var x = content.childNodes;
 		content.removeChild(content.firstChild);
 		del();
 	}
 	insert[4].onclick = function(){
-		var x = content.childNodes;
 		content.removeChild(content.lastChild);
 		del();
+	}
+	insert[5].onclick = function(){
+		content.innerHTML = null;
+		for(var i=0;i<60;i++){
+			num = Math.round(Math.random()*90+10);
+			content.innerHTML += "<div>"+num+"</div>";
+			var dd = content.getElementsByTagName("div");
+			dd[i].style.height = num+"px";
+			del();
+		}
 	}
 	function del(){
 	 	var dd = content.getElementsByTagName("div");
 	 	for(var i=0;i<dd.length;i++){
 	 		dd[i].onclick = function(){
-				var x = this.parentNode;
-       			x.removeChild(this);
-				del();
+       			this.parentNode.removeChild(this);
 	 		}
 	 	}
+	}
+	function pan(){
+		var dd = content.childNodes.length;
+		if (dd>60) {
+			alert("超过60了");
+		}
 	}
 
 	}
