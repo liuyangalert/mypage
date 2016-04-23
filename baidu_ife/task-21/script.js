ff746d87f3d2dbc839a7f2249263a77164284744
陶群 <793264946@qq.com>
2016-04-23 16:08:08 +0800

    增加点击删除

--- baidu_ife/task-21/script.js
+++ baidu_ife/task-21/script.js
@@ -5,11 +5,49 @@ document.onkeyup = function(event){
 }
 
 function getvalue(){
+
 	var value = document.getElementById("num");
 	var ul = document.getElementById("tag_ul");
+	var li = document.createElement("li");
+	var leng = ul.getElementsByTagName("li");
 	var s = "";
-	var number = 13;
-	s += "<li>"+value.value+"</li>";
-	ul.innerHTML += s;
-	value.value = null;
+	if(value.value.trim()!==""){
+		if(!ifvalue(ul,value.value)){
+			li.innerHTML = value.value.trim();
+			ul.appendChild(li);
+			value.value = null;
+			if(leng.length > 9){
+				ul.removeChild(ul.firstChild);
+			}
+		}	
+	}
+	del();	
+function ifvalue(tag,txt){
+	var li = tag.getElementsByTagName("li");
+	for(var i=0;i<li.length;i++){
+		if( li[i].innerText == txt ){
+			return true;
+		} else{
+			return false;
+		}
+	}
+}
+function del(){
+	var ul = document.getElementById("tag_ul");
+	var li = ul.getElementsByTagName("li");
+	for( var i=0;i<li.length;i++ ){
+		var s = "";
+		li[i].onclick = function(){
+			ul.removeChild(this);
+		}
+		li[i].onmouseover = function(){
+			s = this.innerText;
+			this.innerText = "删除 "+this.innerText;
+		}
+		li[i].onmouseout = function(){
+			this.innerText = s;
+		}
+	}
+}
+	
 }
