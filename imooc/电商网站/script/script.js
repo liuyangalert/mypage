ca2ce66c85a1f0214198074ecb7414d02da99081
陶群 <793264946@qq.com>
2016-05-16 22:29:00 +0800

    四张轮播图

--- imooc/电商网站/script/script.js
+++ imooc/电商网站/script/script.js
@@ -5,15 +5,29 @@ window.onload=function(){
 function banner_show(){
 	var left_btn = $("#banner_tag_left");
 	var right_btn = $("#banner_tag_right");
-	var banner = $("banner");
+	var banner_w = $("#banner").width();
+
 	$(left_btn).bind("click",function(){
-		$("#banner").animate({
-			left:"-811px",
-		},300)
+		if( $("#banner").css("left") == "-"+(banner_w-811)+"px"){
+			$("#banner").animate({
+				left:"0px",
+			},1000)
+		} else {
+			$("#banner").animate({
+				left:"-=811px"
+			},300)
+		}
 	});
+
 	$(right_btn).bind("click",function(){
-		$("#banner").animate({
-			left:"0px",
-		},300)
+		if($("#banner").css("left") == "0px"){
+			$("#banner").animate({
+				left:"-"+(banner_w-811)+"px"
+			},1000)
+		} else {
+			$("#banner").animate({
+				left:"+=811px"
+			},300)
+		}
 	});
 }
