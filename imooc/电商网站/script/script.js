d5d219c5490ae6f982c1cc18e508d819add613df
陶群 <793264946@qq.com>
2016-05-18 21:00:16 +0800

    完善轮播

--- imooc/电商网站/script/script.js
+++ imooc/电商网站/script/script.js
@@ -17,7 +17,9 @@ function banner_show(){
 		$("#banner").animate({
 			left:-Owidth*index+"px",
 		},300)
+		banner_icon();
 	});
+
 	$(right_btn).bind("click",function(){
 		index--;
 		if( index<0){
@@ -26,7 +28,9 @@ function banner_show(){
 		$("#banner").animate({
 			left:-Owidth*index+"px",
 		},300)
+		banner_icon();
 	});
+
 	setInterval(function(){
 		index++;
 		if( index>(Oindex.length-1)){
@@ -38,5 +42,24 @@ function banner_show(){
 		$("#banner").animate({
 			left:-Owidth*index+"px",
 		},300)
-	},3000)
+		banner_icon();
+	},5000)
+
+	$(".banner_icon").bind("click",function(){
+		$this = $(this);
+		index = $this.html() - 1;
+		$("#banner").animate({
+			left:-Owidth*index+"px",
+		},300)
+		$(".banner_icon").removeClass("banner_icon_active");
+		$this.addClass("banner_icon_active");
+	})
+
+	function banner_icon(){
+	$(".banner_icon").removeClass("banner_icon_active");
+	var x = document.getElementsByClassName("banner_icon");
+	for( var i=0;i<x.length;i++ ){
+		$(x[index]).addClass("banner_icon_active");
+	}
+	}
 }
