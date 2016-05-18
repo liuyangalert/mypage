375e8fd7e2abe89ddd46bb71f4240ec72d9b3018
陶群 <793264946@qq.com>
2016-05-18 23:40:01 +0800

    完成 小图轮播

--- imooc/电商网站/script/script.js
+++ imooc/电商网站/script/script.js
@@ -1,5 +1,7 @@
 window.onload=function(){
 	banner_show();
+	content_ad("left_ad1");
+	content_ad("left_ad2");
 }
 
 function banner_show(){
@@ -8,48 +10,55 @@ function banner_show(){
 	var Owidth = $("#banner ul li").width();
 	var Oindex = $("#banner ul li");
 	var banner_w = $("#banner").width();
- 	var index = 0;
+ 	var num = 0;
 	$(left_btn).bind("click",function(){
-		index++;
-		if( index>(Oindex.length-1)){
-			index = 0;
+		num++;
+		if( num>(Oindex.length-1)){
+			num = 0;
 		}
 		$("#banner").animate({
-			left:-Owidth*index+"px",
+			left:-Owidth*num+"px",
 		},300)
 		banner_icon();
 	});
 
 	$(right_btn).bind("click",function(){
-		index--;
-		if( index<0){
-			index=Oindex.length-1;
+		num--;
+		if( num<0){
+			num=Oindex.length-1;
 		}
 		$("#banner").animate({
-			left:-Owidth*index+"px",
+			left:-Owidth*num+"px",
 		},300)
 		banner_icon();
 	});
+	var timer = setInterval(function(){
+					num++;
+					
+					if( num>(Oindex.length-1)){
+						num = 0;
+					}
+					if( num<0){
+						num=Oindex.length-1;
+					}
+					
+					$("#banner").animate({
+						left:-Owidth*num+"px",
+					},300)
+					banner_icon();
+				},3000)
 
-	setInterval(function(){
-		index++;
-		if( index>(Oindex.length-1)){
-			index = 0;
-		}
-		if( index<0){
-			index=Oindex.length-1;
-		}
-		$("#banner").animate({
-			left:-Owidth*index+"px",
-		},300)
-		banner_icon();
-	},5000)
+	$("#main_banner").hover(function(){
+			clearInterval(timer);
+		},function(){
+			banner_show();
+		});
 
 	$(".banner_icon").bind("click",function(){
 		$this = $(this);
-		index = $this.html() - 1;
+		num = $this.html() - 1;
 		$("#banner").animate({
-			left:-Owidth*index+"px",
+			left:-Owidth*num+"px",
 		},300)
 		$(".banner_icon").removeClass("banner_icon_active");
 		$this.addClass("banner_icon_active");
@@ -57,9 +66,23 @@ function banner_show(){
 
 	function banner_icon(){
 	$(".banner_icon").removeClass("banner_icon_active");
-	var x = document.getElementsByClassName("banner_icon");
-	for( var i=0;i<x.length;i++ ){
-		$(x[index]).addClass("banner_icon_active");
+	$(".banner_icon").eq(num).addClass("banner_icon_active");
 	}
+}
+
+function content_ad(id){
+	var id = document.getElementById(id);
+	var x = id.getElementsByClassName("content_left_icon");
+	for(var i=0;i<x.length;i++){
+		x[i].index = i;
+		$(x[i]).bind("click",function(){
+			$this= $(this);
+			$(x).removeClass("content_left_icon_active");
+			$this.addClass("content_left_icon_active");
+			$(id).children(".content_left_ad").children("li").css("z-index","-2");
+			$(id).children(".content_left_ad").children("li").eq(this.index).css("z-index","-1");
+		});
+		
 	}
+		
 }
