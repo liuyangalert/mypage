83bb0fea55ac5755d2530ed9ea1fbd0f4b402d88
陶群 <793264946@qq.com>
2016-05-21 15:40:17 +0800

    完成产品分类页 index-2

---
+++ imooc/电商网站/script/index-2.js
@@ -0,0 +1,70 @@
+window.onload=function(){
+	banner_show();
+}
+
+function banner_show(){
+	var left_btn = $("#banner_tag_left");
+	var right_btn = $("#banner_tag_right");
+	var Owidth = $("#banner ul li").width();
+	var Oindex = $("#banner ul li");
+	var banner_w = $("#banner").width();
+ 	var num = 0;
+	$(left_btn).bind("click",function(){
+		num++;
+		if( num>(Oindex.length-1)){
+			num = 0;
+		}
+		$("#banner").animate({
+			left:-Owidth*num+"px",
+		},300)
+		banner_icon();
+	});
+
+	$(right_btn).bind("click",function(){
+		num--;
+		if( num<0){
+			num=Oindex.length-1;
+		}
+		$("#banner").animate({
+			left:-Owidth*num+"px",
+		},300)
+		banner_icon();
+	});
+	var timer = setInterval(function(){
+					num++;
+					
+					if( num>(Oindex.length-1)){
+						num = 0;
+					}
+					if( num<0){
+						num=Oindex.length-1;
+					}
+					
+					$("#banner").animate({
+						left:-Owidth*num+"px",
+					},300)
+					banner_icon();
+				},3000)
+
+	$("#main_banner").hover(function(){
+			clearInterval(timer);
+		},function(){
+			banner_show();
+		});
+
+	$(".banner_icon").bind("click",function(){
+		$this = $(this);
+		num = $this.html() - 1;
+		$("#banner").animate({
+			left:-Owidth*num+"px",
+		},300)
+		$(".banner_icon").removeClass("banner_icon_active");
+		$this.addClass("banner_icon_active");
+	})
+
+	function banner_icon(){
+	$(".banner_icon").removeClass("banner_icon_active");
+	$(".banner_icon").eq(num).addClass("banner_icon_active");
+	}
+}
+	
