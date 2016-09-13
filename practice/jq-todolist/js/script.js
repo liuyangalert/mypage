a33d35085f9dd9c9cfae386a350d3ff5cd85a8d7
taoqun <793264946@qq.com>
2016-09-13 13:46:23 +0800

    add

--- practice/jq-todolist/js/script.js
+++ practice/jq-todolist/js/script.js
@@ -1,12 +1,54 @@
 $('document').ready(function(){
-    $('.checkbox').click(function(){
-        $('.checkbox').toggleClass('checked');
-        if($('.checkbox').hasClass('checked')){
-            console.log(1);
-            $('.checkbox').parent().find('.task-content').css('text-decoration','line-through');
-        }else{
-            console.log(2);
-            $('.checkbox').parent().find('.task-content').css('text-decoration','none');
+
+    addfn();
+    function addfn(){
+        var taskList = $('.task-list .task');
+        $(taskList).each(function(){
+
+            $(this).find('.checkbox').click(function(){
+                $(this).parent().toggleClass('tasked');
+            });
+
+            $(this).find('.task-close').bind('click',function(){
+                $(this).parent().remove();
+            });
+
+            $(this).find('.task-content').bind('keydown',function(event){
+                event = event || window.event;
+                if(event.keyCode === 13){
+                    $(this).parent().next().find('.task-content').focus();
+                    return false;
+                }
+            })
+        });
+    }
+
+    function addTask(){
+        var str = $('.add-task .input-info').val();
+        if(str !== ''){
+            var template = $('<div class="task">\
+            <span class="checkbox"></span>\
+            <span class="task-close"></span>\
+            <div class="task-content" contenteditable="true">'+str+'</div>\
+            </div>');
+            $('.task-list').prepend(template);
         }
+        $('.input-info').eq(0).val('');
+        addfn();
+    }
+
+    $('.btn-sbt').bind('click',function(){
+        addTask();
     });
+
+    $('.add-task .input-info').bind('focus',function(){
+        $(this).bind('keyup',function(event){
+            event = event || widnow.event;
+            if(event.keyCode === 13){
+                addTask();
+            }
+        })
+    })
+
+
 });
